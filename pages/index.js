b0294bf8482573b65df24a3917553a79b9cffd65
dariosandison <dario.sandison@gmail.com>
2025-08-30 08:47:58 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding: "2rem", fontFamily: "sans-serif"}}>
-      <h1>Eco + Holistic Blog</h1>
-      <p>Welcome 🌱 Your site is live and running on Next.js + Vercel!</p>
-    </div>
-  );
-}
