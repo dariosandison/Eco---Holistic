0ba4cdc2b9297cd71117aec92f925818faf00e47
dariosandison <dario.sandison@gmail.com>
2025-08-31 14:17:44 +0100

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -24,3 +24,23 @@ export async function getStaticProps({ params }) {
   const post = await getPostBySlug(params.slug);
   return { props: { post } };
 }
+import Head from "next/head";
+
+// inside your Post component:
+<Head>
+  <title>{post.title} | Wild & Well</title>
+  <meta name="description" content={post.description || "Read more on holistic living and eco-friendly health."} />
+
+  {/* Open Graph */}
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content={`${post.title} | Wild & Well`} />
+  <meta property="og:description" content={post.description} />
+  <meta property="og:url" content={`https://www.wild-and-well.store/posts/${post.slug}`} />
+  <meta property="og:image" content="https://www.wild-and-well.store/cover.jpg" />
+
+  {/* Twitter */}
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={`${post.title} | Wild & Well`} />
+  <meta name="twitter:description" content={post.description} />
+  <meta name="twitter:image" content="https://www.wild-and-well.store/cover.jpg" />
+</Head>
