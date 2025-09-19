8c3a8f20229b6efecc16045fa52a307bca363774
dariosandison <dario.sandison@gmail.com>
2025-09-19 22:19:34 +0100

    chore(seo): remove dynamic robots/sitemap

--- pages/robots.txt.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(
-`User-agent: *
-Allow: /
-
-Sitemap: https://www.wild-and-well.store/sitemap.xml`
-  );
-}
