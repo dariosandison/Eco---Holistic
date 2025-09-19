d63ef2e1d967784ed2a32615cc83b051bac01fee
dariosandison <dario.sandison@gmail.com>
2025-09-19 21:15:16 +0100

    Create DisclosureNote.js

---
+++ components/DisclosureNote.js
@@ -0,0 +1,15 @@
+// components/DisclosureNote.js
+export default function DisclosureNote() {
+  return (
+    <div style={{
+      fontSize: '12px',
+      color: '#666',
+      borderTop: '1px solid #eee',
+      padding: '12px 16px',
+      marginTop: '32px'
+    }}>
+      As an Amazon Associate, we earn from qualifying purchases. This comes at no
+      extra cost to you. Read our <a href="/disclosure">Affiliate Disclosure</a>.
+    </div>
+  );
+}
