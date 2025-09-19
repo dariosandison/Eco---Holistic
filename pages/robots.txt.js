ac06a4bc91d3e3e1e90e4f82fea74a952ed5dc2f
dariosandison <dario.sandison@gmail.com>
2025-09-19 17:19:27 +0100

    Update robots.txt.js

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -1,1 +1,15 @@
+export async function getServerSideProps({ res }) {
+  const site = process.env.NEXT_PUBLIC_SITE_URL || "https://www.example.com";
+  const body = `User-agent: *
+Allow: /
+
+Sitemap: ${site}/sitemap.xml
+`;
+  res.setHeader("Content-Type", "text/plain");
+  res.write(body);
+  res.end();
+  return { props: {} };
+}
+
+export default function Robots() { return null; }
 
