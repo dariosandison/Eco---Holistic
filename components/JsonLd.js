3b63489f4d854163990bbcb1b72519a64ac73c23
dariosandison <dario.sandison@gmail.com>
2025-09-20 11:49:31 +0100

    Update JsonLd.js

--- components/JsonLd.js
+++ components/JsonLd.js
@@ -1,1 +1,11 @@
+// components/JsonLd.js
+export default function JsonLd({ data, id }) {
+  return (
+    <script
+      type="application/ld+json"
+      id={id}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
 
