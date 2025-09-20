b4125b8200588add5046a7d35b83103bc0270e15
dariosandison <dario.sandison@gmail.com>
2025-09-20 12:13:09 +0100

    Create JsonLd.js

---
+++ components/JsonLd.js
@@ -0,0 +1,10 @@
+// components/JsonLd.js
+export default function JsonLd({ data, id }) {
+  return (
+    <script
+      type="application/ld+json"
+      id={id}
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+    />
+  );
+}
