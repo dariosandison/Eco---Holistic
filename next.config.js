3521a0ad619dc61c27b0ce73cb5e5249ff532a66
dariosandison <dario.sandison@gmail.com>
2025-09-20 22:29:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'm.media-amazon.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' }
-    ]
-  }
+      { protocol: "https", hostname: "m.media-amazon.com" },
+      { protocol: "https", hostname: "images-na.ssl-images-amazon.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "cdn.pixabay.com" },
+    ],
+  },
 };
 
 module.exports = nextConfig;
