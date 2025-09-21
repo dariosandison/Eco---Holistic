165f8fb673dc15d45e5747f2684e59b97b6e440d
dariosandison <dario.sandison@gmail.com>
2025-09-21 10:56:36 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#0b3d2e" />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Font performance */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Social defaults */}
+        <meta property="og:site_name" content="Wild & Well" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body>
         <Main />
         <NextScript />
