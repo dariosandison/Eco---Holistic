9f9e917a91c9f836a5a232dbbf5a76360ced6f4a
dariosandison <dario.sandison@gmail.com>
2025-09-21 23:38:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Tell Next to output a fully static site (replaces `next export`)
-  output: 'export',
-
-  // If any page uses `next/image`, this makes images work in static export
-  images: { unoptimized: true },
-
-  reactStrictMode: true,
-  trailingSlash: false,
+  output: 'export',              // replaces `next export`
+  images: { unoptimized: true }, // allow images with static export
+  trailingSlash: false
 };
 
 module.exports = nextConfig;
