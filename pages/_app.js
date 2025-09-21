6104a9f3c98ea699805635ef1129f8fac22e2aa2
dariosandison <dario.sandison@gmail.com>
2025-09-21 11:28:03 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,10 @@
 // pages/_app.js
-import "../styles/globals.css";
+import "@/styles/globals.css";
 import Head from "next/head";
 import Script from "next/script";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
-import Nav from "../components/Nav";
-import Footer from "../components/Footer";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url) => gtag.pageview(url);
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => router.events.off("routeChangeComplete", handleRouteChange);
-  }, [router.events]);
+  const GA = process.env.NEXT_PUBLIC_GA_ID || "";
 
   return (
     <>
@@ -29,24 +18,23 @@ export default function App({ Component, pageProps }) {
         )}
       </Head>
 
-      {gtag.GA_ID && (
+      {/* GA4 (loads only if an ID is set) */}
+      {GA && (
         <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="ga4" strategy="afterInteractive">{`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_ID}', { anonymize_ip: true, page_path: window.location.pathname });
-          `}</Script>
+          <Script async src={`https://www.googletagmanager.com/gtag/js?id=${GA}`} />
+          <Script id="ga-setup">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA}', { anonymize_ip: true });
+            `}
+          </Script>
         </>
       )}
 
-      <Nav />
+      <a href="#main" className="skip-link">Skip to content</a>
       <Component {...pageProps} />
-      <Footer />
     </>
   );
 }
