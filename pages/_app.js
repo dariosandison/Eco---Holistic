ed04bb33b37ab522a2d080e1d13e78583539d4e3
dariosandison <dario.sandison@gmail.com>
2025-09-22 11:54:04 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,34 @@
-import '../styles/globals.css';
-import Layout from '../src/components/Layout';
+import '@/styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { GA_ID, pageview } from '@/src/lib/gtag';
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
+
   return (
-    <Layout>
+    <>
+      {GA_ID && (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+          <Script id="ga" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { send_page_view: true });
+            `}
+          </Script>
+        </>
+      )}
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
