1daba07ae54be550cfaf579d5754a1d1d6dbeb52
dariosandison <dario.sandison@gmail.com>
2025-09-22 08:17:42 +0100

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import Header from './Header';
+import Footer from './Footer';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <main className="container">{children}</main>
+      <Footer />
+    </>
+  );
+}
