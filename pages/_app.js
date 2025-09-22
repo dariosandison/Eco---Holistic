4b162184303603423b1c6d9c3c1f7658e0dd9b6c
dariosandison <dario.sandison@gmail.com>
2025-09-22 16:29:29 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,41 @@
-import "@/styles/globals.css";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import Script from "next/script";
-import * as gtag from "@/src/lib/gtag";
+// pages/_app.js
+import '../styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import * as gtag from '../src/lib/gtag';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    if (!gtag.GA_ID) return;
     const handleRouteChange = (url) => gtag.pageview(url);
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => router.events.off("routeChangeComplete", handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
   }, [router.events]);
 
   return (
     <>
+      {/* GA4 loader */}
       {gtag.GA_ID && (
         <>
           <Script
+            id="ga4-src"
             src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_ID}`}
             strategy="afterInteractive"
           />
-          <Script id="gtag-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              window.gtag = gtag;
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_ID}', { page_path: window.location.pathname });
-            `}
-          </Script>
+          <Script id="ga4-init" strategy="afterInteractive">{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_ID}', {
+              anonymize_ip: true,
+              page_path: window.location.pathname
+            });
+          `}</Script>
         </>
       )}
       <Component {...pageProps} />
     </>
   );
 }
-
