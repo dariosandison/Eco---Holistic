f8c0ec4240ad4be183dcf375f797344e2c879cef
dariosandison <dario.sandison@gmail.com>
2025-09-22 12:38:48 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,26 @@
-import '@/styles/globals.css';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import { GA_ID, pageview } from '@/src/lib/gtag';
+import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import * as gtag from "@/src/lib/gtag";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url) => pageview(url);
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => router.events.off('routeChangeComplete', handleRouteChange);
+    if (!gtag.GA_ID) return;
+    const handleRouteChange = (url) => gtag.pageview(url);
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
   }, [router.events]);
 
   return (
     <>
-      {GA_ID && (
+      {gtag.GA_ID && (
         <>
-          <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
-          <Script id="ga" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_ID}', { send_page_view: true });
-            `}
-          </Script>
-        </>
-      )}
-      <Component {...pageProps} />
-    </>
-  );
-}
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="gtag-init" strategy="afterInteractive">
+            {
