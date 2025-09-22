1ef2a81ffd3c4dc5cbc1e034e9f5b91475b68883
dariosandison <dario.sandison@gmail.com>
2025-09-22 08:16:37 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,44 +1,10 @@
 import '../styles/globals.css';
-import Head from 'next/head';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Layout from '../src/components/Layout';
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID; // optional
-
-export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  // Simple GA pageview tracking (optional)
-  useEffect(() => {
-    if (!GA_ID) return;
-    const handleRouteChange = (url) => {
-      window.gtag?.('config', GA_ID, { page_path: url });
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => router.events.off('routeChangeComplete', handleRouteChange);
-  }, [router.events]);
-
+export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {GA_ID && (
-          <>
-            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}');
-                `
-              }}
-            />
-          </>
-        )}
-      </Head>
+    <Layout>
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
