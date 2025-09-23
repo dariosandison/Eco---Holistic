bff30f7cb170aee4def718cb20dab63f861a0143
dariosandison <dario.sandison@gmail.com>
2025-09-23 23:57:50 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,26 @@
 // pages/_app.js
+import "../styles/globals.css";
 import Head from "next/head";
 import Script from "next/script";
-import { useEffect } from "react";
-import "../styles/globals.css";
-import Newsletter from "../components/Newsletter";
+import SiteFooter from "../components/SiteFooter";
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID || ""; // already set earlier
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID || "G-0G3ER4B1RE";
 
-function gtag(...args) {
-  if (typeof window !== "undefined" && window.dataLayer) {
-    window.dataLayer.push(args);
-  }
-}
-
-export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // basic pageview on route change (optional)
-    const handleRoute = (url) => {
-      if (!GA_ID) return;
-      window.gtag?.("config", GA_ID, { page_path: url });
-    };
-    if (typeof window !== "undefined") {
-      window.addEventListener("hashchange", () => handleRoute(location.pathname + location.hash));
-    }
-    return () => window.removeEventListener("hashchange", () => {});
-  }, []);
-
+export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      {/* Google Analytics (uses GA4 id you already added) */}
+      {/* GA4 */}
       {GA_ID ? (
         <>
           <Script
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
             strategy="afterInteractive"
           />
-          <Script id="gtag-init" strategy="afterInteractive">
+          <Script id="ga4" strategy="afterInteractive">
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
@@ -50,11 +31,8 @@ export default function MyApp({ Component, pageProps }) {
         </>
       ) : null}
 
-      {/* Page content */}
       <Component {...pageProps} />
-
-      {/* Site-wide email capture (sticky bar + footer block) */}
-      <Newsletter />
+      <SiteFooter />
     </>
   );
 }
