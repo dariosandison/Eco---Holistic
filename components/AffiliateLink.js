ad4ef22d5e08e3d5bd3dad7983d0a91e3ad0a946
dariosandison <dario.sandison@gmail.com>
2025-09-24 23:56:29 +0100

    Update AffiliateLink.js

--- components/AffiliateLink.js
+++ components/AffiliateLink.js
@@ -1,5 +1,4 @@
 export default function AffiliateLink({ href, children, ...props }) {
-  // Always mark affiliate/sponsored links
   const rel = ['nofollow', 'sponsored', 'noopener', 'noreferrer'].join(' ');
   return (
     <a href={href} rel={rel} target="_blank" {...props}>
