f19284e6c9ba40d14d459db9b147befd34a575b0
dariosandison <dario.sandison@gmail.com>
2025-09-24 23:47:05 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,20 +1,17 @@
-// components/Header.js
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="border-b bg-white">
-      <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-3">
-        <Link href="/" className="flex items-center gap-3">
-          <img src="/logo.svg" alt="Wild & Well" className="h-7 w-auto" />
-          <span className="sr-only">Wild & Well</span>
+    <header className="site-header">
+      <div className="container header-inner">
+        <Link href="/" className="brand">
+          Wild & Well
         </Link>
-
-        <nav className="flex items-center gap-6 text-sm">
-          <Link href="/guides" className="hover:underline">Guides</Link>
-          <Link href="/deals" className="hover:underline">Deals</Link>
-          <Link href="/blog" className="hover:underline">Blog</Link>
-          <Link href="/about" className="hover:underline">About</Link>
+        <nav className="nav">
+          <Link href="/">Home</Link>
+          <Link href="/guides">Guides</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/deals">Deals</Link>
         </nav>
       </div>
     </header>
