4b4c47b965b2f2b82cb36ca94da26d30cf5e5a8c
dariosandison <dario.sandison@gmail.com>
2025-09-25 16:29:23 +0100

    Delete components/JsonLd.jsx

--- components/JsonLd.jsx
+++
@@ -1,9 +0,0 @@
-export default function JsonLd({ data, id }) {
-  return (
-    <script
-      type="application/ld+json"
-      id={id}
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-    />
-  );
-}
