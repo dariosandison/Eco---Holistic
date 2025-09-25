0eb2bfdd4bebfb130388f219ff0e736d94c79758
dariosandison <dario.sandison@gmail.com>
2025-09-25 15:28:19 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+// pages/_app.js
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  // Support per-page custom layouts if a page exports getLayout
+  const getLayout =
+    Component.getLayout || ((page) => <Layout>{page}</Layout>);
+
+  return getLayout(<Component {...pageProps} />);
 }
