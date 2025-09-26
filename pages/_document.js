0ad7f1163962650520fee1da9af411b6f9da5deb
dariosandison <dario.sandison@gmail.com>
2025-09-26 16:46:18 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,19 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
+import site from '@/../site.config';
 
 export default function Document() {
-  const siteName = 'Wild & Well';
-  const theme = '#0f1a0d';
-
   return (
     <Html lang="en">
       <Head>
-        {/* PWA / icons */}
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content={theme} />
-        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
-        <link rel="icon" href="/icon-192.png" sizes="192x192" />
-        <link rel="icon" href="/icon-512.png" sizes="512x512" />
-        <link rel="apple-touch-icon" href="/icon-192.png" />
-
-        {/* Preload brand font(s) here if you add any later */}
-
-        {/* Basic meta fallback */}
-        <meta name="application-name" content={siteName} />
-        <meta name="apple-mobile-web-app-title" content={siteName} />
-        <meta name="color-scheme" content="light dark" />
+        <meta name="theme-color" content={site.color} />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        {/* add your font link if needed */}
       </Head>
-      <body className="bg-white text-slate-900 antialiased">
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
-
