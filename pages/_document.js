79e6320349bb978d62d2d36693e98bb0f3898ffc
dariosandison <dario.sandison@gmail.com>
2025-09-26 17:41:07 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,11 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
-import site from '@/../site.config';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="theme-color" content={site.color} />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        {/* add your font link if needed */}
-      </Head>
-      <body className="antialiased">
+      <Head />
+      <body>
         <Main />
         <NextScript />
       </body>
