f92abb0e26ae365ae9eba3a5a1b3a913d034905d
dariosandison <dario.sandison@gmail.com>
2025-09-26 17:30:50 +0100

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -1,44 +1,29 @@
 // components/SEO.js
 import Head from 'next/head';
-import site from '@/../site.config';
+import site from '@/site.config';
 
-export default function SEO({
-  title,
-  description,
-  image,
-  canonical,
-  noindex = false,
-}) {
-  const metaTitle = title ? `${title} • ${site.shortName}` : site.defaultTitle;
-  const metaDesc = description || site.defaultDescription;
-  const metaImg = image || site.defaultImage;
-  const url = canonical || site.domain;
+export default function SEO({ title, description, image, canonical, noindex }) {
+  const t = title ? `${title} | ${site.name}` : site.seoDefaults.title || site.name;
+  const d = description || site.seoDefaults.description;
+  const img = image || site.seoDefaults.image;
 
   return (
     <Head>
-      <title>{metaTitle}</title>
-      <meta name="description" content={metaDesc} />
-      <meta name="theme-color" content={site.color} />
+      <title>{t}</title>
+      {d && <meta name="description" content={d} />}
+      {canonical && <link rel="canonical" href={canonical} />}
       {noindex && <meta name="robots" content="noindex,nofollow" />}
 
-      {/* Canonical */}
-      <link rel="canonical" href={url} />
-
-      {/* Open Graph */}
-      <meta property="og:type" content="website" />
       <meta property="og:site_name" content={site.name} />
-      <meta property="og:title" content={metaTitle} />
-      <meta property="og:description" content={metaDesc} />
-      <meta property="og:image" content={metaImg} />
-      <meta property="og:url" content={url} />
+      <meta property="og:title" content={t} />
+      {d && <meta property="og:description" content={d} />}
+      {img && <meta property="og:image" content={img} />}
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content={site.locale || 'en_US'} />
 
-      {/* Twitter */}
-      <meta name="twitter:card" content="summary_large_image" />
       {site.twitter && <meta name="twitter:site" content={site.twitter} />}
-      <meta name="twitter:title" content={metaTitle} />
-      <meta name="twitter:description" content={metaDesc} />
-      <meta name="twitter:image" content={metaImg} />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="theme-color" content={site.themeColor || '#C9D8B6'} />
     </Head>
   );
 }
