80cab08b3a76f148459faaa9769d56a47e160048
dariosandison <dario.sandison@gmail.com>
2025-09-27 10:20:36 +0100

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,9 @@
-export const seo = { title: 'Contact' };
+// pages/contact.js
 export default function Contact() {
   return (
-    <div className="prose">
+    <div>
       <h1>Contact</h1>
-      <p>Email us: <a href="mailto:hello@yourdomain.com">hello@yourdomain.com</a></p>
+      <p>Email: <a href="mailto:hello@wild-and-well.store">hello@wild-and-well.store</a></p>
     </div>
   );
 }
