54e302093c72878f66ed2d08785e70f2d73e4395
dariosandison <dario.sandison@gmail.com>
2025-09-27 10:55:24 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,18 @@
-export const seo = { title: 'About' };
-export default function About() {
+// pages/_app.js
+import Head from 'next/head';
+import '../styles/globals.css';
+import Layout from '../components/Layout';
+
+export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="prose">
-      <h1>About Wild &amp; Well</h1>
-      <p>We research, test, and recommend low-tox, high-value products.</p>
-    </div>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Wild & Well</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
