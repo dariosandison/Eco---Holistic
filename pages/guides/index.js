14a3b4bf417dc5579e7a12f3989e75171d61fba3
dariosandison <dario.sandison@gmail.com>
2025-09-27 09:25:34 +0100

    Update index.js

--- pages/guides/index.js
+++ pages/guides/index.js
@@ -1,8 +1,3 @@
-- import { getAllDocs } from '@/lib/content';
-+ // TEMP: unblock build while content lib is fixed
-+ const getAllDocs = () => [];
-
-export async function getStaticProps() {
-  const guides = getAllDocs({ dir: 'content/guides', fields: ['title','excerpt','date','slug'] });
-  return { props: { guides } };
-}
+- // TEMP: unblock build while content lib is fixed
+- const getAllDocs = () => [];
++ import { getAllDocs } from '@/lib/content';
