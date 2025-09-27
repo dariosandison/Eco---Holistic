74fbd24dc2008f6e22b0e7be219d80f6993e70b9
dariosandison <dario.sandison@gmail.com>
2025-09-27 11:02:31 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,15 @@
-// pages/_app.js
-import Head from 'next/head';
-import '../styles/globals.css';
-import Layout from '../components/Layout';
-
-export default function MyApp({ Component, pageProps }) {
+// pages/about.js
+export default function About() {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Wild & Well</title>
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <div>
+      <h1>About Wild & Well</h1>
+      <p>
+        Wild & Well shares practical wellness guides and clean product picks.
+        We research, test, and curate so you can feel better with fewer, better things.
+      </p>
+      <p>
+        Questions? Email <a href="mailto:hello@wild-and-well.store">hello@wild-and-well.store</a>.
+      </p>
+    </div>
   );
 }
