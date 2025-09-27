eb18fdd50de30d1ade501ab654f2f4d1c9ad6f17
dariosandison <dario.sandison@gmail.com>
2025-09-27 23:28:36 +0100

    Update disclaimer.js

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -1,18 +1,3 @@
-// pages/disclaimer.js
-import { getDocBySlug } from '../lib/content';
-import { renderMarkdown } from '../lib/markdown';
-
-export async function getStaticProps() {
-  const fields = ['slug', 'title', 'date', 'content', 'excerpt'];
-  const doc =
-    getDocBySlug({ dir: 'content/legal', slug: 'disclaimer', fields }) ||
-    getDocBySlug({ dir: '.', slug: 'disclaimer', fields });
-
-  if (!doc) return { notFound: true };
-  const html = renderMarkdown(doc.content || '');
-  return { props: { doc, html } };
-}
-
 export default function Disclaimer() { return null; }
 
 export async function getStaticProps() {
