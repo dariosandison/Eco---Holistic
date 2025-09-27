6b7943928a60434516a179f1699e6a5224a00311
dariosandison <dario.sandison@gmail.com>
2025-09-27 23:28:11 +0100

    Update cookies.js

--- pages/cookies.js
+++ pages/cookies.js
@@ -1,18 +1,3 @@
-// pages/cookies.js
-import { getDocBySlug } from '../lib/content';
-import { renderMarkdown } from '../lib/markdown';
-
-export async function getStaticProps() {
-  const fields = ['slug', 'title', 'date', 'content', 'excerpt'];
-  const doc =
-    getDocBySlug({ dir: 'content/legal', slug: 'cookies', fields }) ||
-    getDocBySlug({ dir: '.', slug: 'cookies', fields });
-
-  if (!doc) return { notFound: true };
-  const html = renderMarkdown(doc.content || '');
-  return { props: { doc, html } };
-}
-
 export default function Cookies() { return null; }
 
 export async function getStaticProps() {
@@ -23,3 +8,4 @@ export async function getStaticProps() {
     },
   };
 }
+
