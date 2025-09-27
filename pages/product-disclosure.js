41b5a8f794c13baea7c0445a60619a434732a314
dariosandison <dario.sandison@gmail.com>
2025-09-27 23:29:05 +0100

    Update product-disclosure.js

--- pages/product-disclosure.js
+++ pages/product-disclosure.js
@@ -1,18 +1,3 @@
-// pages/product-disclosure.js
-import { getDocBySlug } from '../lib/content';
-import { renderMarkdown } from '../lib/markdown';
-
-export async function getStaticProps() {
-  const fields = ['slug', 'title', 'date', 'content', 'excerpt'];
-  const doc =
-    getDocBySlug({ dir: 'content/legal', slug: 'product-disclosure', fields }) ||
-    getDocBySlug({ dir: '.', slug: 'product-disclosure', fields });
-
-  if (!doc) return { notFound: true };
-  const html = renderMarkdown(doc.content || '');
-  return { props: { doc, html } };
-}
-
 export default function ProductDisclosure() { return null; }
 
 export async function getStaticProps() {
