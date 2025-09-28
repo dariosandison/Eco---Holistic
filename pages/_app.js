1e8b20e231f43c38acb84aefafffcd1fb6c8db7d
dariosandison <dario.sandison@gmail.com>
2025-09-28 16:55:39 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,16 @@
 import '../styles/globals.css';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
+// inside <Head> in _app.js or _document.js
+<script type="application/ld+json" dangerouslySetInnerHTML={{
+  __html: JSON.stringify({
+    "@context":"https://schema.org",
+    "@graph":[
+      organization({ name:"Wild & Well", url:"https://www.wild-and-well.store", logo:"/logo.svg" }),
+      website({ name:"Wild & Well", url:"https://www.wild-and-well.store" })
+    ]
+  })
+}} />
 
 export default function MyApp({ Component, pageProps }) {
   return (
