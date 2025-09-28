50d65c3145e8e2c26a1d85156aad0dc44945cf17
dariosandison <dario.sandison@gmail.com>
2025-09-28 18:01:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    // Easiest way to ensure all guide/blog images render without remote config headaches
-    unoptimized: true,
-  },
-  reactStrictMode: true,
+  async redirects() {
+    return [
+      { source: '/affiliate-disclosure', destination: '/legal/affiliate-disclosure', permanent: true },
+      { source: '/product-disclosure', destination: '/legal/product-disclosure', permanent: true },
+      { source: '/cookies', destination: '/legal/cookies', permanent: true },
+      { source: '/disclaimer', destination: '/legal/disclaimer', permanent: true }
+    ];
+  }
 };
 
 module.exports = nextConfig;
