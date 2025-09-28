5e80142efaaabf59e08c96b39c7078c04b267254
dariosandison <dario.sandison@gmail.com>
2025-09-28 20:14:36 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,64 +1,36 @@
 // pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-const ORG = {
-  "@context":"https://schema.org",
-  "@type":"Organization",
-  name:"Wild & Well",
-  url:"https://www.wild-and-well.store",
-  logo:"https://www.wild-and-well.store/logo.svg"
-};
-const WEBSITE = {
-  "@context":"https://schema.org",
-  "@type":"WebSite",
-  name:"Wild & Well",
-  url:"https://www.wild-and-well.store",
-  potentialAction:{
-    "@type":"SearchAction",
-    target:"https://www.wild-and-well.store/search?q={search_term_string}",
-    "query-input":"required name=search_term_string"
-  }
-};
+export default function Document() {
+  return (
+    <Html lang="en" data-theme="wildandwell">
+      <Head>
+        {/* Color & rendering hints */}
+        <meta name="theme-color" content="#fdf6e5" />
+        <meta name="color-scheme" content="light only" />
 
-export default class MyDocument extends Document {
-  render() {
-    const GA = process.env.NEXT_PUBLIC_GA_ID;
-    return (
-      <Html lang="en">
-        <Head>
-          {/* Performance: preconnect for GA if enabled */}
-          {GA ? <link rel="preconnect" href="https://www.googletagmanager.com" /> : null}
-          {GA ? <link rel="dns-prefetch" href="https://www.googletagmanager.com" /> : null}
+        {/* Preconnects for analytics (safe even if GA is disabled) */}
+        <link rel="preconnect" href="https://www.google-analytics.com" crossOrigin="" />
+        <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
 
-          {/* Site-wide JSON-LD (Organization + Website) */}
-          <script
-            type="application/ld+json"
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: JSON.stringify({"@context":"https://schema.org","@graph":[ORG, WEBSITE]}) }}
-          />
-          {/* GA4 (loads only if NEXT_PUBLIC_GA_ID is set) */}
-          {GA && (
-            <>
-              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA}`} />
-              <script
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${GA}', { anonymize_ip: true, send_page_view: true });
-                  `,
-                }}
-              />
-            </>
-          )}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        {/* Preload the homepage LCP (logo) */}
+        <link
+          rel="preload"
+          as="image"
+          href="/logo.svg"
+          type="image/svg+xml"
+          imagesrcset="/logo.svg"
+          fetchpriority="high"
+        />
+
+        {/* Favicon basics */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <noscript><div style={{display:'none'}}>JavaScript disabled</div></noscript>
+      </body>
+    </Html>
+  );
 }
