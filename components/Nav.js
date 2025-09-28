a725765e756b07dd9400d6d23e362d63eeb421c4
dariosandison <dario.sandison@gmail.com>
2025-09-28 20:16:07 +0100

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,20 +1,29 @@
 // components/Nav.js
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Nav() {
   return (
     <header className="site-header">
       <div className="container header-inner">
         <Link href="/" className="logo-link" aria-label="Wild & Well Home">
-          <img src="/logo.svg" alt="" className="site-logo" />
+          <Image
+            src="/logo.svg"
+            alt="Wild & Well"
+            width={26}
+            height={26}
+            priority
+            fetchPriority="high"
+            aria-hidden="true"
+          />
           <span>Wild &amp; Well</span>
         </Link>
 
         <nav className="nav" aria-label="Primary">
           <Link href="/guides">Guides</Link>
           <Link href="/reviews">Reviews</Link>
-          <Link href="/deals">Deals</Link>
           <Link href="/blog">Blog</Link>
+          <Link href="/deals">Deals</Link>
           <Link href="/contact">Contact</Link>
         </nav>
 
