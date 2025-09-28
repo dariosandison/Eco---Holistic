4af023acc5f46ce2432fe54ea81379819f55a823
dariosandison <dario.sandison@gmail.com>
2025-09-28 17:50:31 +0100

    Create MDXComponents.js

---
+++ components/MDXComponents.js
@@ -0,0 +1,32 @@
+// components/MDXComponents.js
+import Link from 'next/link';
+import AffiliateLink from './AffiliateLink';
+import ProsCons from './ProsCons';
+import ComparisonTable from './ComparisonTable';
+import Disclosure from './Disclosure';
+
+const A = (props) => {
+  const href = props.href || '';
+  const isExternal = /^https?:\/\//i.test(href);
+  if (isExternal) {
+    return <a target="_blank" rel="nofollow noopener" {...props} />;
+  }
+  return <Link href={href}>{props.children}</Link>;
+};
+
+const H2 = (props) => <h2 {...props} style={{ marginTop: 22, marginBottom: 8 }} />;
+const H3 = (props) => <h3 {...props} style={{ marginTop: 18, marginBottom: 6 }} />;
+const P  = (props) => <p  {...props} style={{ margin: '10px 0' }} />;
+
+export const mdxComponents = {
+  a: A,
+  h2: H2,
+  h3: H3,
+  p: P,
+
+  // Custom components you can embed directly inside content
+  AffiliateLink,
+  ProsCons,
+  ComparisonTable,
+  Disclosure
+};
