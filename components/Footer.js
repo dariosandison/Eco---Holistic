50e82070ba8a1a72609d5b150c095bc9225c40c7
dariosandison <dario.sandison@gmail.com>
2025-09-28 18:02:11 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,12 +7,11 @@ export default function Footer() {
     <footer className="site-footer site-footer--fixed" role="contentinfo">
       <div className="container footer-bar">
         <span className="footer-copy">© {year} Wild &amp; Well</span>
-
         <ul className="legal-list" aria-label="Legal links">
-          <li><Link href="/affiliate-disclosure">Affiliate Disclosure</Link></li>
-          <li><Link href="/product-disclosure">Product Disclosure</Link></li>
-          <li><Link href="/cookies">Cookies</Link></li>
-          <li><Link href="/disclaimer">Disclaimer</Link></li>
+          <li><Link href="/legal/affiliate-disclosure">Affiliate Disclosure</Link></li>
+          <li><Link href="/legal/product-disclosure">Product Disclosure</Link></li>
+          <li><Link href="/legal/cookies">Cookies</Link></li>
+          <li><Link href="/legal/disclaimer">Disclaimer</Link></li>
         </ul>
       </div>
     </footer>
