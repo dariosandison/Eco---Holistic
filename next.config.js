458848321903eca1f392fc832a85d06c5cedac77
dariosandison <dario.sandison@gmail.com>
2025-09-28 20:01:54 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
       { source: '/affiliate-disclosure', destination: '/legal/affiliate-disclosure', permanent: true },
       { source: '/product-disclosure', destination: '/legal/product-disclosure', permanent: true },
       { source: '/cookies', destination: '/legal/cookies', permanent: true },
-      { source: '/disclaimer', destination: '/legal/disclaimer', permanent: true }
+      { source: '/disclaimer', destination: '/legal/disclaimer', permanent: true },
+      { source: '/privacy', destination: '/legal/privacy', permanent: true }
     ];
   }
 };
