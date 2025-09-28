f966acede9b1d80c3b426ec992284646abf5e63c
dariosandison <dario.sandison@gmail.com>
2025-09-28 16:52:43 +0100

    Update AffiliateLink.js

--- components/AffiliateLink.js
+++ components/AffiliateLink.js
@@ -1,11 +1,24 @@
-export default function AffiliateLink({ href, children, className = "", ...props }) {
-  const safeHref = href || "#";
+import { event as gaEvent } from '../lib/gtag';
+
+export default function AffiliateLink({ href = "#", children, className = "", onClick, ...props }) {
+  const handleClick = (e) => {
+    try {
+      gaEvent({
+        action: 'affiliate_click',
+        category: 'affiliate',
+        label: href,
+        value: 1
+      });
+    } catch {}
+    onClick?.(e);
+  };
   return (
     <a
-      href={safeHref}
+      href={href}
       target="_blank"
       rel="nofollow sponsored noopener noreferrer"
       className={`underline ${className}`}
+      onClick={handleClick}
       {...props}
     >
       {children}
