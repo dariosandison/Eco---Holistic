8d5eeabd5ac067c0a3f6e8e89d770c569ea32dde
dariosandison <dario.sandison@gmail.com>
2025-09-28 21:55:32 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,52 +1,24 @@
 // pages/_app.js
-import '../styles/globals.css';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Script from 'next/script';
+import Analytics from '../components/Analytics';
 import * as gtag from '../lib/gtag';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
-  // Track pageviews without blocking navigation
   useEffect(() => {
-    const onRoute = (url) => gtag.pageview(url);
-    router.events.on('routeChangeComplete', onRoute);
-    return () => router.events.off('routeChangeComplete', onRoute);
+    const handleRouteChange = (url) => gtag.pageview(url);
+    router.events.on('routeChangeComplete', handleRouteChange);
+    // fire once on initial load
+    gtag.pageview(window.location.pathname + window.location.search);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
   }, [router.events]);
 
   return (
     <>
-      {/* GA4 (loads after interactive to avoid delaying LCP) */}
-      {gtag.GA_MEASUREMENT_ID ? (
-        <>
-          <Script
-            id="ga4-src"
-            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="ga4-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag.GA_MEASUREMENT_ID}', {
-                anonymize_ip: true
-              });
-            `}
-          </Script>
-        </>
-      ) : null}
-
-      <div className="page-shell">
-        <Nav />
-        <main className="page-main">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-      </div>
+      <Analytics />
+      <Component {...pageProps} />
     </>
   );
 }
