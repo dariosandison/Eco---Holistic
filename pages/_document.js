b2157630260377acbb3d287328d8bc1f8f603496
dariosandison <dario.sandison@gmail.com>
2025-09-28 22:15:29 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,36 +1,26 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en" data-theme="wildandwell">
-      <Head>
-        {/* Color & rendering hints */}
-        <meta name="theme-color" content="#fdf6e5" />
-        <meta name="color-scheme" content="light only" />
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en-GB">
+        <Head>
+          {/* Performance hints */}
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
+          <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
+          <link rel="preconnect" href="https://www.google-analytics.com" />
+          <link rel="dns-prefetch" href="https://www.google-analytics.com" />
 
-        {/* Preconnects for analytics (safe even if GA is disabled) */}
-        <link rel="preconnect" href="https://www.google-analytics.com" crossOrigin="" />
-        <link rel="preconnect" href="https://www.googletagmanager.com" crossOrigin="" />
-
-        {/* Preload the homepage LCP (logo) */}
-        <link
-          rel="preload"
-          as="image"
-          href="/logo.svg"
-          type="image/svg+xml"
-          imagesrcset="/logo.svg"
-          fetchpriority="high"
-        />
-
-        {/* Favicon basics */}
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-        <noscript><div style={{display:'none'}}>JavaScript disabled</div></noscript>
-      </body>
-    </Html>
-  );
+          {/* UX */}
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="color-scheme" content="light dark" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
