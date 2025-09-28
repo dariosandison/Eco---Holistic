7162f508505284aace66413d57791e829d76cba2
dariosandison <dario.sandison@gmail.com>
2025-09-28 20:34:03 +0100

    Update and rename Disclosure.js to Disclosure.jsx

--- components/Disclosure.js
+++
@@ -1,8 +0,0 @@
-// components/Disclosure.js
-export default function Disclosure({ className = "" }) {
-  return (
-    <div className={`disclosure ${className}`}>
-      <strong>Heads up:</strong> We’re reader-supported. If you buy through our links, we may earn a commission. We only recommend products we genuinely use or research deeply.
-    </div>
-  );
-}
