e032c380f989cc3112400380e7319541af96f838
dariosandison <dario.sandison@gmail.com>
2025-09-28 09:49:24 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'm.media-amazon.com' },
-      { protocol: 'https', hostname: 'images-na.ssl-images-amazon.com' },
-      { protocol: 'https', hostname: 'i5.walmartimages.com' },
-      { protocol: 'https', hostname: 'cdn.shopify.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'img.youtube.com' },
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-    ],
+    // Easiest way to ensure all guide/blog images render without remote config headaches
+    unoptimized: true,
   },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
