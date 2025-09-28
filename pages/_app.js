e096030f4f7b796e2a167b27bf3f11c61399006c
dariosandison <dario.sandison@gmail.com>
2025-09-28 22:11:33 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,39 @@
 // pages/_app.js
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Analytics from '../components/Analytics';
-import * as gtag from '../lib/gtag';
+import Script from 'next/script';
+import { GA_ID, pageview } from '../lib/gtag';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url) => gtag.pageview(url);
+    if (!GA_ID) return;
+    const handleRouteChange = (url) => pageview(url);
     router.events.on('routeChangeComplete', handleRouteChange);
-    // fire once on initial load
-    gtag.pageview(window.location.pathname + window.location.search);
     return () => router.events.off('routeChangeComplete', handleRouteChange);
   }, [router.events]);
 
   return (
     <>
-      <Analytics />
+      {/* GA script (only if ID provided) */}
+      {GA_ID ? (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              window.gtag = gtag;
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}', { anonymize_ip: true });
+            `}
+          </Script>
+        </>
+      ) : null}
       <Component {...pageProps} />
     </>
   );
