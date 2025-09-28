a24d59c9634b67a3cac8bddf20e69999082300b2
dariosandison <dario.sandison@gmail.com>
2025-09-28 15:33:53 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,6 @@
 // pages/_app.js
 import '../styles/global.css';
-import Header from '../components/Header';
-import NewsletterBar from '../components/NewsletterBar';
-import Footer from '../components/Footer';
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <div className="app-shell">
-      <Header />
-      <NewsletterBar />
-      <main className="app-main">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
