490c797039449ebe2e326a5a68ecb6e85c9d0088
dariosandison <dario.sandison@gmail.com>
2025-09-29 16:48:53 +0100

    Update and rename SmartLink.js to SmartLink.jsx

--- components/SmartLink.jsx
+++ components/SmartLink.jsx
@@ -1,21 +1,25 @@
-// components/SmartLink.js
+// components/SmartLink.jsx
 import Link from 'next/link';
 
-export default function SmartLink({ href = '', children, ...props }) {
+export default function SmartLink({ href = '', children, ...rest }) {
   const isInternal = href && (href.startsWith('/') || href.startsWith('#'));
+
   if (isInternal) {
+    // Internal links use Next.js <Link> for client-side navigation
     return (
-      <Link href={href} {...props}>
+      <Link href={href} {...rest}>
         {children}
       </Link>
     );
   }
+
+  // External/affiliate links are always nofollow+sponsored
   return (
     <a
       href={href}
       target="_blank"
       rel="nofollow sponsored noopener noreferrer"
-      {...props}
+      {...rest}
     >
       {children}
     </a>
