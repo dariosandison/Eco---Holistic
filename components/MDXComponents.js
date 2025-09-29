be7ad510b0aa6a709f66742cb9516d1a64989e27
dariosandison <dario.sandison@gmail.com>
2025-09-29 21:03:33 +0100

    Update MDXComponents.js

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -4,6 +4,7 @@ import AffiliateLink from './mdx/AffiliateLink';
 import BuyBox from './mdx/BuyBox';
 import ProsCons from './mdx/ProsCons';
 import Disclosure from './mdx/Disclosure';
+import ComparisonTable from './mdx/ComparisonTable';
 
 const A = ({ href = '', children, className = '', ...rest }) => {
   const isExternal = /^https?:\/\//i.test(href);
@@ -33,6 +34,9 @@ const MDXComponents = {
   BuyBox,
   ProsCons,
   Disclosure,
+  ComparisonTable,
 };
 
+// Support both default export and named `mdxComponents` for existing imports.
+export const mdxComponents = MDXComponents;
 export default MDXComponents;
