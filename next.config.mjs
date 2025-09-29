dc68766214a258d02f85f57ebafc9c3848edb36d
dariosandison <dario.sandison@gmail.com>
2025-09-29 21:21:47 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: false,
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images-na.ssl-images-amazon.com' },
       { protocol: 'https', hostname: 'm.media-amazon.com' },
       { protocol: 'https', hostname: 'cdn.shopify.com' },
       { protocol: 'https', hostname: '**.amazonaws.com' },
-      { protocol: 'https', hostname: '**.cloudfront.net' },
-    ],
+      { protocol: 'https', hostname: '**.cloudfront.net' }
+    ]
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' }
+        ]
+      }
+    ];
+  }
 };
 export default nextConfig;
