b82658af020b243ae9981862c10d12b197b561f5
dariosandison <dario.sandison@gmail.com>
2025-09-29 20:16:34 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,13 @@
 // pages/_app.js
-import '../styles/ui.css';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import { GA_ID, pageview } from '../lib/gtag';
+import React from 'react';
+import { MDXProvider } from '@mdx-js/react';
+import MDXComponents from '../components/MDXComponents';
 
-export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!GA_ID) return;
-    const handleRouteChange = (url) => pageview(url);
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => router.events.off('routeChangeComplete', handleRouteChange);
-  }, [router.events]);
-
+export default function MyApp({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => page);
   return (
-    <>
-      {GA_ID ? (
-        <>
-          <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            strategy="afterInteractive"
-          />
-          <Script id="ga-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              window.gtag = gtag;
-              gtag('js', new Date());
-              gtag('config', '${GA_ID}', { anonymize_ip: true });
-            `}
-          </Script>
-        </>
-      ) : null}
-      <Component {...pageProps} />
-    </>
+    <MDXProvider components={MDXComponents}>
+      {getLayout(<Component {...pageProps} />)}
+    </MDXProvider>
   );
 }
