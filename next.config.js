ae1e44bf4e78ab40ba920fc0fff19b7aea96e653
dariosandison <dario.sandison@gmail.com>
2025-09-29 16:57:18 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-
-  // We’re shipping a fully static site (compatible with your export flow)
-  output: 'export',
-
-  // Use the raw <img> output for next/image so we don’t need remote loaders/domains
-  images: {
-    unoptimized: true
-  }
+  // Keep this if you’re using external images without a loader/domains setup
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
