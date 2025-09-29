29e05074ec9f965e37a4388b92b86721992f7970
dariosandison <dario.sandison@gmail.com>
2025-09-29 17:13:01 +0100

    Update and rename MDXComponents.js to MDXComponents.jsx

---
+++ components/MDXComponents.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Minimal MDX components map.
+ * We export the named `mdxComponents` to satisfy imports like:
+ *   import { mdxComponents } from '../components/MDXComponents'
+ *
+ * Leaving it empty preserves your existing MDX content exactly as-is,
+ * without altering rendering. You can add custom mappings here later.
+ */
+const mdxComponents = {};
+
+export default mdxComponents;
+export { mdxComponents };
