c8474dba792d03beb757e46e24b065f684f23b3d
dariosandison <dario.sandison@gmail.com>
2025-09-29 16:56:32 +0100

    Update MDXComponents.js

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -1,15 +1,12 @@
-// components/MDXComponents.js
+// components/MDXComponents.jsx
 import NextImage from 'next/image';
 import SmartLink from './SmartLink';
 
-// Optional custom components you already have; keep imports if present.
-// If you don’t have these components, you can safely remove their lines here.
+// Optional: keep/remove if you don't have these
 import Callout from './Callout';
 import CompareInline from './CompareInline';
 import CompareTable from './CompareTable';
 
-// Image component that avoids CLS and works with static export.
-// If width/height aren’t provided in MDX, we fall back to a plain <img>.
 function MdxImage(props) {
   const { src = '', alt = '', width, height, ...rest } = props;
 
@@ -39,15 +36,14 @@ function MdxImage(props) {
   );
 }
 
-const MDXComponents = {
+const mdxComponents = {
   a: SmartLink,
   img: (props) => <MdxImage {...props} />,
   Image: (props) => <MdxImage {...props} />,
-
-  // Only exported if they exist in your repo
   Callout,
   CompareInline,
   CompareTable
 };
 
-export default MDXComponents;
+export default mdxComponents;   // default import support
+export { mdxComponents };       // named import support
