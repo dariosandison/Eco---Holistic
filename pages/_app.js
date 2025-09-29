f4f336053ad361016fc8d7df0076f1cbf518be35
dariosandison <dario.sandison@gmail.com>
2025-09-29 21:02:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 // pages/_app.js
 import '../styles/globals.css';
 import { MDXProvider } from '@mdx-js/react';
-import MDXComponents from '../components/MDXComponents';
+import MDXComponents, { mdxComponents } from '../components/MDXComponents';
 
 function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
+  // Provide components both globally and via prop so pages using either pattern work.
   return (
     <MDXProvider components={MDXComponents}>
-      {getLayout(<Component {...pageProps} />)}
+      {getLayout(<Component {...pageProps} components={mdxComponents} />)}
     </MDXProvider>
   );
 }
