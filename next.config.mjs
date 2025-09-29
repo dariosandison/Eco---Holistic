dc6b1e33c5cee23c78405f72b09d7cfde9f68ca9
dariosandison <dario.sandison@gmail.com>
2025-09-29 22:14:50 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,18 @@ const nextConfig = {
         headers: [
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' }
         ]
+      },
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }
+        ]
+      },
+      {
+        source: '/(.*)\\.(?:js|css|png|jpg|jpeg|gif|svg|webp|ico|woff2)$',
+        headers: [
+          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }
+        ]
       }
     ];
   }
