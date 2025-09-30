e7f1427717556a3634deada9d1d1995cb578409c
dariosandison <dario.sandison@gmail.com>
2025-09-30 21:21:47 +0100

    Create index.js

---
+++ components/mdx/index.js
@@ -0,0 +1,11 @@
+import AffiliateLink from './AffiliateLink';
+import BuyBox from './BuyBox';
+
+// Add any other custom MDX components here and they'll be available in MDX.
+const mdxComponents = {
+  AffiliateLink,
+  BuyBox,
+};
+
+export { AffiliateLink, BuyBox };
+export default mdxComponents;
