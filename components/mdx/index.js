6b882acde8ec8ca08fa04a3d39cafcdc36aac1b6
dariosandison <dario.sandison@gmail.com>
2025-10-01 09:07:21 +0100

    Update index.js

--- components/mdx/index.js
+++ components/mdx/index.js
@@ -1,11 +1,16 @@
 import AffiliateLink from './AffiliateLink';
 import BuyBox from './BuyBox';
+import ComparisonTable from './ComparisonTable';
+import Disclosure from './Disclosure';
 
 // Add any other custom MDX components here and they'll be available in MDX.
 const mdxComponents = {
   AffiliateLink,
   BuyBox,
+  ComparisonTable,
+  Disclosure,
 };
 
-export { AffiliateLink, BuyBox };
+export { AffiliateLink, BuyBox, ComparisonTable, Disclosure };
 export default mdxComponents;
+
