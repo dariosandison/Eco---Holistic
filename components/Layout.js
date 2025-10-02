cf553a6bc363789c1cf19cd842851d6816cddaf6
dariosandison <dario.sandison@gmail.com>
2025-10-02 21:20:57 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,11 @@
 import SiteHeader from "./SiteHeader";
 import SiteFooter from "./SiteFooter";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }) {
   return (
     <>
       <SiteHeader />
-      <main className="container" style={{paddingTop:24, paddingBottom:96}}>
+      <main className="container" style={{ paddingTop: 24, paddingBottom: 96 }}>
         {children}
       </main>
       <SiteFooter />
