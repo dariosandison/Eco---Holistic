9a021823346f2e448a02d0e693d89f5cba290c48
dariosandison <dario.sandison@gmail.com>
2025-10-08 17:43:33 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-module.exports = {
+// postcss.config.js (ESM-friendly)
+// Your package.json has `"type": "module"`, so use ESM export here.
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
