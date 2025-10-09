dc26b362e4368333c547b5526fe548fb2f5e30ae
dariosandison <dario.sandison@gmail.com>
2025-10-09 17:11:46 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "../styles/globals.css";
+import SiteHeader from "../components/SiteHeader";
+import SiteFooter from "../components/SiteFooter";
+import NewsletterBar from "../components/NewsletterBar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SiteHeader />
+      <NewsletterBar />
+      <main className="min-h-screen bg-brand-green text-brand-cream">
+        <Component {...pageProps} />
+      </main>
+      <SiteFooter />
+    </>
+  );
 }
