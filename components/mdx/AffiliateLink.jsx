428e2590a3bd49e2d66e8bffbf1f1f0c900f72b2
dariosandison <dario.sandison@gmail.com>
2025-10-11 00:03:07 +0100

    Update AffiliateLink.jsx

--- components/mdx/AffiliateLink.jsx
+++ components/mdx/AffiliateLink.jsx
@@ -1,9 +1,17 @@
+'use client'
 import { withAmazonTag } from '@/lib/affiliates'
+import { trackAffiliateClick } from '@/lib/analytics'
 
 export default function AffiliateLink({ href = '#', children }) {
   const url = withAmazonTag(href)
   return (
-    <a href={url} target="_blank" rel="noopener nofollow sponsored" className="underline decoration-dotted">
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener nofollow sponsored"
+      className="underline decoration-dotted"
+      onClick={() => trackAffiliateClick({ href: url, text: typeof children === 'string' ? children : undefined })}
+    >
       {children}
     </a>
   )
